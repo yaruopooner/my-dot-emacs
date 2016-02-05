10c4f5a45b313f79c0a50b32864312ef468a0921
yaruopooner <yaruopooner@gmail.com>
2016-02-06 03:07:33 +0900

    modified coding value

--- .emacs.d/.emacs24/packages/user/uenox-dired.el
+++ .emacs.d/.emacs24/packages/user/uenox-dired.el
@@ -1,64 +1,64 @@
-;;; -*- Mode: Emacs-Lisp ; Coding: iso-2022-jp-dos -*-
+;;; -*- Mode: Emacs-Lisp ; coding: utf-8-unix -*-
 ;;
-;; uenox-dired.el : Dired $B%b!<%I$r3HD%$9$k(B Emacs Lisp $B%9%/%j%W%H(B
+;; uenox-dired.el : Dired モードを拡張する Emacs Lisp スクリプト
 ;; Copyright (C) 2000 UENO Tetsuyuki
 ;;
-;; $BK\%W%m%0%i%`$O%U%j!<!&%=%U%H%&%'%"$G$9!#(B
-;; $B!!$"$J$?$O!"(BFree Software Foundation$B$,8xI=$7$?(BGNU $B0lHL8xM-;HMQ5vBz$N(B
-;;$B!V%P!<%8%g%s#2!W0?$$$O$=$l0J9_$N3F%P!<%8%g%s$NCf$+$i$$$:$l$+$rA*Br$7!"(B
-;; $B$=$N%P!<%8%g%s$,Dj$a$k>r9`$K=>$C$FK\%W%m%0%i%`$r:FHRI[$^$?$OJQ99$9$k(B
-;; $B$3$H$,$G$-$^$9!#(B
-;; $B!!K\%W%m%0%i%`$OM-MQ$H$O;W$$$^$9$,!"HRI[$K$"$?$C$F$O!";T>l@-5Z$SFCDj(B
-;; $BL\E*E,9g@-$K$D$$$F$N0EL[$NJ]>Z$r4^$a$F!"$$$+$J$kJ]>Z$b9T$J$$$^$;$s!#(B
-;; $B>\:Y$K$D$$$F$O(BGNU $B0lHL8xM-;HMQ5vBz=q$r$*FI$_$/$@$5$$!#(B
-;; $B!!$"$J$?$O!"K\%W%m%0%i%`$H0l=o$K(BGNU$B0lHL8xM-;HMQ5vBz$N<L$7$r<u$1<h$C$F(B
-;; $B$$$k$O$:$G$9!#$=$&$G$J$$>l9g$O!"(B
+;; 本プログラムはフリー・ソフトウェアです。
+;; 　あなたは、Free Software Foundationが公表したGNU 一般公有使用許諾の
+;;「バージョン２」或いはそれ以降の各バージョンの中からいずれかを選択し、
+;; そのバージョンが定める条項に従って本プログラムを再頒布または変更する
+;; ことができます。
+;; 　本プログラムは有用とは思いますが、頒布にあたっては、市場性及び特定
+;; 目的適合性についての暗黙の保証を含めて、いかなる保証も行ないません。
+;; 詳細についてはGNU 一般公有使用許諾書をお読みください。
+;; 　あなたは、本プログラムと一緒にGNU一般公有使用許諾の写しを受け取って
+;; いるはずです。そうでない場合は、
 ;;   Free Software Foundation, Inc.,
 ;;   59 Temple Place - Suite 330, Boston, MA 02111-1307, USA 
-;; $B$X<j;f$r=q$$$F$/$@$5$$!#(B
+;; へ手紙を書いてください。
 ;;
-;; $BCx:n<T(B: $B>eLn(B $BE/9,(B "UENO Tetsuyuki" <uenox@livedoor.com>
-;; $BHG>pJs(B: 1.00
-;; $B:n@.F|;~!'(B2000/10/21
-;; $BG[I[>l=j(B: http://uenox.hoops.livedoor.com/elisp/uenox-dired.el
+;; 著作者: 上野 哲幸 "UENO Tetsuyuki" <uenox@livedoor.com>
+;; 版情報: 1.00
+;; 作成日時：2000/10/21
+;; 配布場所: http://uenox.hoops.livedoor.com/elisp/uenox-dired.el
 ;;
 ;;
-;;$B!ZF3F~![(B
-;;  $B>e5-$NG[I[>l=j$+$i%U%!%$%k$r%@%&%s%m!<%I$7$F!"%[!<%`%G%#%l%/%H%j(B("~")
-;;  $B$K3JG<$7$F$/$@$5$$!#(B
+;;【導入】
+;;  上記の配布場所からファイルをダウンロードして、ホームディレクトリ("~")
+;;  に格納してください。
 ;;
-;;$B!Z@_Dj![(B
-;;  $B=i4|2=%U%!%$%k(B .emacs $B$K0J2<$N9T$r=q$-2C$($F$/$@$5$$!#(B
+;;【設定】
+;;  初期化ファイル .emacs に以下の行を書き加えてください。
 ;;    (setq load-path (cons "~" load-path))
 ;;    (load "uenox-dired")
 ;;
-;;$B!Z@bL@![(B
-;;  Dired $B$K0J2<$N(B3$B$D$N5!G=$,DI2C$5$l$^$9!#!J%-!<%^%C%W$O$3$N%U%!%$%k$N(B
-;;  $B:G8e$NJ}$r=q$-49$($k$3$H$GJQ99$G$-$^$9!#!K(B
+;;【説明】
+;;  Dired に以下の3つの機能が追加されます。（キーマップはこのファイルの
+;;  最後の方を書き換えることで変更できます。）
 ;;   * C-j 
-;;     $B8=:_$N9T$N%U%!%$%k$r(BWindows$B$K4XO"IU$1$i$l$?%"%W%j%1!<%7%g%s$r;H$C(B
-;;     $B$F3+$-$^$9!#!J$o$6$o$6%(%/%9%W%m!<%i!<$r3+$+$J$/$FNI$/$J$j$^$9!#!K(B
+;;     現在の行のファイルをWindowsに関連付けられたアプリケーションを使っ
+;;     て開きます。（わざわざエクスプローラーを開かなくて良くなります。）
 ;;   * w
-;;     $B8=:_$N9T$N%U%!%$%kL>$r(BUNIX$B7A<0$K$F%3%T!<$7$^$9!#(B
+;;     現在の行のファイル名をUNIX形式にてコピーします。
 ;;   * W
-;;     $B8=:_$N9T$N%U%!%$%kL>$r(BDOS$B7A<0$K$F%3%T!<$7$^$9!#(B
+;;     現在の行のファイル名をDOS形式にてコピーします。
 ;;
-;;$B!Z(BTips$B![(B
-;;  $B!!%U%!%$%k$rJL$N%G%#%l%/%H%j$K%3%T!<$7$?$$$H$-$K!"$^$:%3%T!<@h$N%G%#(B
-;;$B!!%l%/%H%j$r(BDired$B$G3+$$$F!H(B.$B!I$NItJ,$K%+!<%=%k$r;}$C$F$$$F(B w $B$r2!$7$^(B
-;;$B!!$9!#<!$K!"%3%T!<$7$?$$%U%!%$%k$,3JG<$5$l$F$$$k%G%#%l%/%H%j$G%U%!%$%k(B
-;;$B!!$N0LCV$K%+!<%=%k$r;}$C$F$$$F(B C $B$r2!$7$^$9!#%_%K%P%C%U%!$,3+$/$N$G!"(B
-;;$B!!(BC-y $B$H$7$F%3%T!<@h$N%G%#%l%/%H%jL>$r%d%s%/$7$?8e(B Enter $B$r2!$7$^$9!#(B
+;;【Tips】
+;;  　ファイルを別のディレクトリにコピーしたいときに、まずコピー先のディ
+;;　レクトリをDiredで開いて“.”の部分にカーソルを持っていて w を押しま
+;;　す。次に、コピーしたいファイルが格納されているディレクトリでファイル
+;;　の位置にカーソルを持っていて C を押します。ミニバッファが開くので、
+;;　C-y としてコピー先のディレクトリ名をヤンクした後 Enter を押します。
 ;;
-;;$B!ZCm0U![(B
-;;  (1) $B$3$N%9%/%j%W%H$NCf$G$O(B uenox-dired- $B$H$$$&J8;zNs$+$i;O$^$k%7%s%\(B
-;;    $B%k$r;H$C$F$$$^$9!#(B
-;;  (2) Windows98$B$GHs(BASCII$BJ8;zNs$r;H$C$?%m%0%$%sL>$G%m%0%$%s$7$?>l9g$K$b!"(B
-;;    $B%U%!%$%k$N=jM-<T$,@5>o$KI=<($5$l$k$h$&$K$9$k$?$a!"(Buser-login-name$B$H(B
-;;    user-real-login-name$B$r(Bsjis$B$G(Bdecode$B$7$F$$$^$9!#(B
+;;【注意】
+;;  (1) このスクリプトの中では uenox-dired- という文字列から始まるシンボ
+;;    ルを使っています。
+;;  (2) Windows98で非ASCII文字列を使ったログイン名でログインした場合にも、
+;;    ファイルの所有者が正常に表示されるようにするため、user-login-nameと
+;;    user-real-login-nameをsjisでdecodeしています。
 ;;
 
-;;; Dired $B$G(B Windows $B$K4XO"IU$1$i$l$?%U%!%$%k$r5/F0$9$k!#(B
+;;; Dired で Windows に関連付けられたファイルを起動する。
 (defun uenox-dired-winstart()
   "Type '\\[uenox-dired-winstart]': win-start the current line's file."
   (interactive)
@@ -70,7 +70,7 @@
     )
   )
 
-;;; Dired $B$G(B UNIX $B7A<0$G%U%!%$%kL>$r%3%T!<$9$k!#(B
+;;; Dired で UNIX 形式でファイル名をコピーする。
 (defun uenox-dired-kill-filename-unix()
   "Type '\\[uenox-dired-kill-filename-unix]': copy the current line's filename(unix)."
   (interactive)
@@ -82,7 +82,7 @@
     )
   )
 
-;;; Dired $B$G(B DOS $B7A<0$G%U%!%$%kL>$r%3%T!<$9$k!#(B
+;;; Dired で DOS 形式でファイル名をコピーする。
 (defun uenox-dired-kill-filename-dos()
   "Type '\\[uenox-dired-kill-filename-dos]': copy the current line's filename(dos)."
   (interactive)
@@ -94,7 +94,7 @@
     )
   )
 
-;;; Dired $B$N5/F0;~$K%-!<%\!<%I%^%C%W$r=q$-49$($k!#(B
+;;; Dired の起動時にキーボードマップを書き換える。
 (add-hook 'dired-mode-hook
 	  '(lambda ()
 	     (define-key dired-mode-map "\C-j" 'uenox-dired-winstart)
@@ -103,7 +103,7 @@
 	     )
 	  )
 
-;;; $BHs(BASCII$BJ8;zNs$G%m%0%$%s$7$F$b(B Dired $B$G@5>o$KI=<($5$l$k$h$&$K$7$^$9!#(B
+;;; 非ASCII文字列でログインしても Dired で正常に表示されるようにします。
 (setq user-login-name (decode-coding-string user-login-name 'sjis))
 (setq user-real-login-name (decode-coding-string user-real-login-name 'sjis))
 
--- .emacs.d/.emacs24/packages/user/uenox-ps-print.el
+++ .emacs.d/.emacs24/packages/user/uenox-ps-print.el
@@ -1,74 +1,74 @@
-;;;;; -*- Mode: Emacs-Lisp ; Coding: iso-2022-jp-dos -*-
+;;;;; -*- Mode: Emacs-Lisp ; coding: utf-8-unix -*-
 ;;
-;; uenox-ps-print.el : Windows NT $BI8=`$N(B LPR $B$r;H$C$F(B ps-print $B$9$k(B
+;; uenox-ps-print.el : Windows NT 標準の LPR を使って ps-print する
 ;; Copyright (C) 2000 UENO Tetsuyuki
 ;;
-;; $BK\%W%m%0%i%`$O%U%j!<!&%=%U%H%&%'%"$G$9!#(B
-;; $B!!$"$J$?$O!"(BFree Software Foundation$B$,8xI=$7$?(BGNU $B0lHL8xM-;HMQ5vBz$N(B
-;;$B!V%P!<%8%g%s#2!W0?$$$O$=$l0J9_$N3F%P!<%8%g%s$NCf$+$i$$$:$l$+$rA*Br$7!"(B
-;; $B$=$N%P!<%8%g%s$,Dj$a$k>r9`$K=>$C$FK\%W%m%0%i%`$r:FHRI[$^$?$OJQ99$9$k(B
-;; $B$3$H$,$G$-$^$9!#(B
-;; $B!!K\%W%m%0%i%`$OM-MQ$H$O;W$$$^$9$,!"HRI[$K$"$?$C$F$O!";T>l@-5Z$SFCDj(B
-;; $BL\E*E,9g@-$K$D$$$F$N0EL[$NJ]>Z$r4^$a$F!"$$$+$J$kJ]>Z$b9T$J$$$^$;$s!#(B
-;; $B>\:Y$K$D$$$F$O(BGNU $B0lHL8xM-;HMQ5vBz=q$r$*FI$_$/$@$5$$!#(B
-;; $B!!(BGNU$B0lHL8xM-;HMQ5vBz$N<L$7$O(B http://www.gnu.org/copyleft/gpl.txt $B$+(B
-;; $B$iF~<j$9$k$3$H$,$G$-$^$9!#(B
-;; $B!!F|K\8lHG$K$D$$$F$O(B http://www.gnu.org/copyleft/ $B$r;2>H4j$$$^$9!#(B
+;; 本プログラムはフリー・ソフトウェアです。
+;; 　あなたは、Free Software Foundationが公表したGNU 一般公有使用許諾の
+;;「バージョン２」或いはそれ以降の各バージョンの中からいずれかを選択し、
+;; そのバージョンが定める条項に従って本プログラムを再頒布または変更する
+;; ことができます。
+;; 　本プログラムは有用とは思いますが、頒布にあたっては、市場性及び特定
+;; 目的適合性についての暗黙の保証を含めて、いかなる保証も行ないません。
+;; 詳細についてはGNU 一般公有使用許諾書をお読みください。
+;; 　GNU一般公有使用許諾の写しは http://www.gnu.org/copyleft/gpl.txt か
+;; ら入手することができます。
+;; 　日本語版については http://www.gnu.org/copyleft/ を参照願います。
 ;;
-;; $BCx:n<T(B: $B>eLn(B $BE/9,(B "UENO Tetsuyuki" <uenox@livedoor.com>
-;; $BHG>pJs(B: 0.02
-;; $B:n@.F|;~!'(B2000/10/24
-;; $BG[I[>l=j(B: http://uenox.hoops.livedoor.com/elisp/uenox-ps-print.el
+;; 著作者: 上野 哲幸 "UENO Tetsuyuki" <uenox@livedoor.com>
+;; 版情報: 0.02
+;; 作成日時：2000/10/24
+;; 配布場所: http://uenox.hoops.livedoor.com/elisp/uenox-ps-print.el
 ;;
-;;$B!ZF3F~![(B
-;;  $B!!>e5-$NG[I[>l=j$+$i%U%!%$%k$r%@%&%s%m!<%I$7$F!"%[!<%`%G%#%l%/%H%j(B
-;;  ("~")$B$K3JG<$7$F$/$@$5$$!#(B
+;;【導入】
+;;  　上記の配布場所からファイルをダウンロードして、ホームディレクトリ
+;;  ("~")に格納してください。
 ;;
-;;$B!Z@_Dj![(B
-;;  $B=i4|2=%U%!%$%k(B .emacs $B$K0J2<$N9T$r=q$-2C$($F$/$@$5$$!#(B
+;;【設定】
+;;  初期化ファイル .emacs に以下の行を書き加えてください。
 ;;    (setq load-path (cons "~" load-path))
 ;;    (load "uenox-ps-print")
 ;;
-;;$B!!%G%U%)%k%H$N%W%j%s%?L>$H%W%j%s%?L>$N0lMw$rEPO?$r$9$k0Y!"0J2<$N9T$r(B
-;;$B!!;29M$K=i4|2=%U%!%$%k(B .emacs $B$K=q$-2C$($F$/$@$5$$!#(B
+;;　デフォルトのプリンタ名とプリンタ名の一覧を登録をする為、以下の行を
+;;　参考に初期化ファイル .emacs に書き加えてください。
 ;;    (setq uenox-ps-printer-name "pspr01")
 ;;    (setq uenox-ps-printer-name-list '("pspr01" 
 ;;                                       "pspr02"
 ;;                                       "pspr03"))
 ;;
-;;$B!!(BM-x uenox-select-ps-printer $B$H$9$k$3$H$K$h$j!"EPO?$7$?%W%j%s%?$rA*(B
-;;$B!!Br$9$k$3$H$,$G$-$k$h$&$K$J$j$^$9!#(B
+;;　M-x uenox-select-ps-printer とすることにより、登録したプリンタを選
+;;　択することができるようになります。
 ;;
-;;$B!Z@bL@![(B
-;;  Windows NT $BI8=`$N(B lpr $B$r;H$C$F0u:~$9$k$3$H$,$G$-$k$h$&$K$J$j$^$9!#(B
+;;【説明】
+;;  Windows NT 標準の lpr を使って印刷することができるようになります。
 ;;
-;;$B!Z(BTips$B![(B
-;;  $B"(MQ;f%5%$%:$HMQ;fJ}8~$r@_Dj$9$k$K$O!"0J2<$N$h$&$K@_Dj$7$^$9!#(B
-;;    (setq ps-paper-type 'a4) ; $B%5%$%:$OI,$:@_Dj$7$^$7$g$&!#(B
+;;【Tips】
+;;  ※用紙サイズと用紙方向を設定するには、以下のように設定します。
+;;    (setq ps-paper-type 'a4) ; サイズは必ず設定しましょう。
 ;;    (setq ps-landscape-mode t)
 ;;
-;;  $B"(%Z!<%8$KJ#?tNs$N0u:~$r$9$k>l9g!"0J2<$N?tCM$r@_Dj$7$F$/$@$5$$!#(B
+;;  ※ページに複数列の印刷をする場合、以下の数値を設定してください。
 ;;    (setq ps-number-of-columns 1)
 ;;
-;;  $B"(%+%i!<0u:~BP1~(BPS$B%W%j%s%?!<$K0u:~$9$k$K$O0J2<$N$h$&$K$7$^$9!#(B
+;;  ※カラー印刷対応PSプリンターに印刷するには以下のようにします。
 ;;    (setq ps-print-color-p t)
 ;;
-;;$B!ZCm0U![(B
-;;  (1) $B$3$N%9%/%j%W%H$G$O(B uenox- $B$H$$$&J8;zNs$+$i;O$^$k%7%s%\%k$r;H$$(B
-;;     $B$^$9!#(B
-;;  (2) $B$3$N%9%/%j%W%H$G$O(B ps-print.el $B$N(B ps-print-region-function,  
-;;     ps-printer-name, ps-multibyte-buffer $B$NFbMF$r=q$-49$($^$9!#(B
+;;【注意】
+;;  (1) このスクリプトでは uenox- という文字列から始まるシンボルを使い
+;;     ます。
+;;  (2) このスクリプトでは ps-print.el の ps-print-region-function,  
+;;     ps-printer-name, ps-multibyte-buffer の内容を書き換えます。
 ;;
 
-;;; ps-printer $B$NL>>N(B
+;;; ps-printer の名称
 (defvar uenox-ps-printer-name nil
   "The name of a network PostScript Printer.")
 
-;;; ps-printer $B$N%j%9%H(B
+;;; ps-printer のリスト
 (defvar uenox-ps-printer-name-list nil
   "The list of network PostScript Printers.")
 
-;;; ps-printer $B$rA*Br$9$k4X?t(B
+;;; ps-printer を選択する関数
 (defun uenox-select-ps-printer()
   "Select PostScript Printer for output."
   (interactive)
@@ -77,7 +77,7 @@
 		     uenox-ps-printer-name 
 		     'uenox-ps-printer-name-list)))
 
-;;; ps-print.el $B$,Ds6!$9$k4X?t(B ps-print-region-function $B$NBeBX4X?t(B
+;;; ps-print.el が提供する関数 ps-print-region-function の代替関数
 (defun uenox-ps-print-region-function(from to &rest arg)
   "For Windows NT LPR which cannot use pipe!"
   (let ((tmpfile 
@@ -93,13 +93,13 @@
 				   tmpfile)))
 	(delete-file tmpfile)))
 
-;;; ps-print.el $B$,Ds6!$9$k4X?t$r>e5-$N4X?t$G6/@)E*$KCV$-49$($k!#(B
+;;; ps-print.el が提供する関数を上記の関数で強制的に置き換える。
 (setq ps-print-region-function 'uenox-ps-print-region-function)
 
-;;; ps-printer.el $B$N%W%j%s%?L>$r(B($BG0$N0Y(B)$B6u$K$7$F$*$/!#(B
+;;; ps-printer.el のプリンタ名を(念の為)空にしておく。
 (setq ps-printer-name nil)
 
-;;; ps-printer.el $B$N@_Dj$rJQ99$7!"%^%k%A%P%$%H$N0u:~$r2DG=$K$9$k!#(B
+;;; ps-printer.el の設定を変更し、マルチバイトの印刷を可能にする。
 (setq ps-multibyte-buffer 'non-latin-printer)
 
 
