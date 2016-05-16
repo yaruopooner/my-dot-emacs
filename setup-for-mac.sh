80567ae503e687444079e2609507ea5f2db9bc45
yaruopooner <yaruopooner@gmail.com>
2016-05-17 03:01:50 +0900

    update

--- setup-for-mac.sh
+++ setup-for-mac.sh
@@ -1,3 +1,11 @@
 # -*- mode: shell-script ; coding: utf-8-unix -*-
 #! /bin/sh
 
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew tap caskroom/fonts
+brew cask install font-inconsolata
+
+brew tap railwaycat/emacsmacport
+brew install --with-cocoa --srgb --gnutls emacs
+brew linkapps
--- setup-for-ubuntu.sh
+++ setup-for-ubuntu.sh
@@ -1,15 +1,25 @@
 # -*- mode: shell-script ; coding: utf-8-unix -*-
 #! /bin/sh
 
+# sudo -E add-apt-repository ppa:ubuntu-elisp/ppa
 sudo -E apt-get -y update
+
+sudo -E apt-get -y install gnome-color-chooser
+
+sudo -E apt-get -y install fonts-inconsolata
+
+# sudo -E apt-get -y install git
 sudo -E apt-get -y install subversion
 sudo -E apt-get -y install cmake
+
 sudo -E apt-get -y install build-essential
 sudo -E apt-get -y build-dep emacs24
-sudo -E apt-get -y install fonts-inconsolata
-# sudo -E apt-get -y install git
+
+# sudo -E apt-get -y install emacs24 emacs24-el emacs24-common-non-dfsg
+
 mkdir work
 pushd work
+
 wget http://ftp.gnu.org/gnu/emacs/emacs-24.5.tar.gz 
 tar -zxf emacs-24.5.tar.gz
 cd emacs-24.5
