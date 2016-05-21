d3b365650d41f9126dac772bf46d7688283c3a00
yaruopooner <yaruopooner@gmail.com>
2016-05-22 06:16:55 +0900

    modified

--- installer/setup-for-fedora.sh
+++ installer/setup-for-fedora.sh
@@ -3,17 +3,27 @@
 
 # sudo -E add-apt-repository ppa:ubuntu-elisp/ppa
 sudo -E dnf -y update
+sudo -E dnf -y upgrade
 
-sudo -E dnf -y install gnome-color-chooser
+sudo -E dnf -y groupinstall "Development Tools"
 
-sudo -E dnf -y install fonts-inconsolata
+# sudo -E dnf -y install gnome-color-chooser
+
+# sudo -E dnf -y install fonts-inconsolata
+sudo -E dnf -y install levien-inconsolata-fonts
 
 # sudo -E dnf -y install git
 sudo -E dnf -y install subversion
 sudo -E dnf -y install cmake
 
-sudo -E dnf -y install build-essential
-sudo -E dnf -y build-dep emacs24
+# sudo -E dnf -y install build-essential
+# sudo -E dnf -y build-dep emacs24
+
+sudo -E dnf -y gcc
+sudo -E dnf -y gcc-c++
+
+sudo -E dnf -y install texinfo libotf-devel ncurses-devel imagemagic imagemagic-devel libxml2 libxml2-devel gnutls-devel
+sudo -E dnf -y install libXpm libXpm-devel libjpeg libjpeg-devel libtiff libtiff-devel giflib giflib-devel libpng libpng-devel libxaw libxaw-devel
 
 # sudo -E dnf -y install emacs24 emacs24-el emacs24-common-non-dfsg
 
@@ -24,4 +34,5 @@ wget http://ftp.gnu.org/gnu/emacs/emacs-24.5.tar.gz
 tar -zxf emacs-24.5.tar.gz
 cd emacs-24.5
 ./configure
+make
 sudo make install
