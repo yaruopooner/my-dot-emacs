14afaa4a7a482daf7e6830221982d3c7fb4e56d1
yaruopooner <yaruopooner@gmail.com>
2016-10-04 02:18:32 +0900

    added check for submodule already checkout

--- setup-submodule.sh
+++ setup-submodule.sh
@@ -6,21 +6,29 @@ git submodule update --recursive
 
 if [ -d .emacs.d/.emacs24 ]; then
     pushd .emacs.d/.emacs24/packages/user/ac-clang
-    git checkout -b develop origin/develop
+    if [ -z "$(ls -A .)" ]; then
+        git checkout -b develop origin/develop
+    fi
     popd
 
     pushd .emacs.d/.emacs24/packages/user/msvc
-    git checkout -b develop origin/develop
+    if [ -z "$(ls -A .)" ]; then
+        git checkout -b develop origin/develop
+    fi
     popd
 fi
 
 if [ -d .emacs.d/.emacs25 ]; then
     pushd .emacs.d/.emacs25/packages/user/ac-clang
-    git checkout -b develop origin/develop
+    if [ -z "$(ls -A .)" ]; then
+        git checkout -b develop origin/develop
+    fi
     popd
 
     pushd .emacs.d/.emacs25/packages/user/msvc
-    git checkout -b develop origin/develop
+    if [ -z "$(ls -A .)" ]; then
+        git checkout -b develop origin/develop
+    fi
     popd
 fi
 
