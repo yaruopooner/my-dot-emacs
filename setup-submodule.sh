c11fbbdc9a3d6a1c7c5489152421f3045975c4fe
yaruopooner <yaruopooner@gmail.com>
2016-10-04 20:14:57 +0900

    update submodule scripts

--- setup-submodule.sh
+++ setup-submodule.sh
@@ -1,16 +1,21 @@
 # -*- mode: shell-script ; coding: utf-8-unix -*-
 #! /bin/sh
 
-git submodule init
-git submodule update --recursive
+# git submodule init
+git config -f .gitmodules submodule..emacs.d/.emacs24/packages/user/ac-clang.branch develop
+git config -f .gitmodules submodule..emacs.d/.emacs24/packages/user/msvc.branch develop
+git config -f .gitmodules submodule..emacs.d/.emacs25/packages/user/ac-clang.branch develop
+git config -f .gitmodules submodule..emacs.d/.emacs25/packages/user/msvc.branch develop
+# git submodule update --recursive
+git submodule update --init --recursive --remote --merge
 
 if [ -d .emacs.d/.emacs24 ]; then
     pushd .emacs.d/.emacs24/packages/user/ac-clang
-    git checkout -b develop origin/develop
+    # git checkout -b develop origin/develop
     popd
 
     pushd .emacs.d/.emacs24/packages/user/msvc
-    git checkout -b develop origin/develop
+    # git checkout -b develop origin/develop
     popd
 fi
 
@@ -20,7 +25,7 @@ if [ -d .emacs.d/.emacs25 ]; then
     popd
 
     pushd .emacs.d/.emacs25/packages/user/msvc
-    git checkout -b develop origin/develop
+    # git checkout -b develop origin/develop
     popd
 fi
 
---
+++ update-submodule.sh
@@ -0,0 +1,8 @@
+# -*- mode: shell-script ; coding: utf-8-unix -*-
+#! /bin/sh
+
+# git submodule foreach git pull origin master
+git submodule foreach git pull origin develop
+# git submodule update --recursive --remote --merge
+
+
