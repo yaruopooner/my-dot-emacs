4bfd4a71ed828c8bf6e786b645f25f8a4556c9f7
yaruopooner <yaruopooner@gmail.com>
2016-11-07 02:33:12 +0900

    update

--- installer/setup-emacs.sh
+++ installer/setup-emacs.sh
@@ -4,15 +4,14 @@
 
 EMACS_ARCHIVE_URI="http://ftp.gnu.org/gnu/emacs/emacs-25.1.tar.xz"
 readonly EMACS_ARCHIVE_NAME=$( basename "${EMACS_ARCHIVE_URI}" )
-readonly EMACS_VERSION_NAME=$( basename --suffix ".tar.xz" "${EMACS_ARCHIVE_NAME}" )
+readonly EMACS_VERSION_NAME=$( echo "${EMACS_ARCHIVE_NAME}" | sed -r "s/(emacs-[0-9]+\.[0-9]+).*/\1/" )
 
 
-EMACS_ARCHIVE_NAME="emacs-25.1"
 wget --timestamping "${EMACS_ARCHIVE_URI}"
 
 # archive expand
-if [ ! -d "${EMACS_VERSION_NAME}" -a -e "${EMACS_ARCHIVE_NAME}" ]; then
-    tar -Jxvf "${EMACS_ARCHIVE_NAME}"
+if $( [ ! -d "${EMACS_VERSION_NAME}" ] && [ -e "${EMACS_ARCHIVE_NAME}" ] ); then
+    tar -xvf "${EMACS_ARCHIVE_NAME}"
 fi
 
 pushd "${EMACS_VERSION_NAME}"
