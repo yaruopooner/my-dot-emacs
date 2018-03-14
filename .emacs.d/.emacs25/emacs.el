b7f1ee79b5b36ac8cfa77580344eb48c46ca6c57
yaruopooner <yaruopooner@mail.com>
2018-03-14 16:10:38 +0900

    Added package of yasnippet-snippets. yasnippet package divided to yasnippet and yasnippet-snippets.

--- .emacs.d/.emacs25/emacs.el
+++ .emacs.d/.emacs25/emacs.el
@@ -1,5 +1,5 @@
 ;;; -*- mode: emacs-lisp ; coding: utf-8-unix -*-
-;;; last updated : 2018/03/14.16:03:33
+;;; last updated : 2018/03/14.16:10:12
 
 
 ;;==============================================================================
@@ -179,6 +179,7 @@
                             ;; imenu+ ;melpa
 ;;                            hideshowvis ;melpa
                             yasnippet   ;melpa
+                            yasnippet-snippets   ;melpa
 ;;                            dropdown-list ;melpa
                             clang-format  ;melpa
                             ;; ggtags ;melpa
