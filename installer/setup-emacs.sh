d3d34c88db8f0b2fcb72caaa5d058f1f298ee5a5
yaruopooner <yaruopooner@gmail.com>
2018-06-09 03:06:16 +0900

    Updated shells

--- installer/setup-emacs.sh
+++ installer/setup-emacs.sh
@@ -2,7 +2,8 @@
 #! /bin/sh
 
 
-declare -r EMACS_ARCHIVE_URI="http://ftp.gnu.org/gnu/emacs/emacs-25.3.tar.xz"
+declare -r EMACS_ARCHIVE_URI="http://ftp.gnu.org/gnu/emacs/emacs-26.1.tar.xz"
+# declare -r EMACS_ARCHIVE_URI="http://ftp.gnu.org/gnu/emacs/emacs-25.3.tar.xz"
 declare -r EMACS_ARCHIVE_NAME=$( basename "${EMACS_ARCHIVE_URI}" )
 declare -r EMACS_VERSION_NAME=$( echo "${EMACS_ARCHIVE_NAME}" | sed -r "s/(emacs-[0-9]+\.[0-9]+).*/\1/" )
 
@@ -17,10 +18,10 @@ fi
 pushd "${EMACS_VERSION_NAME}"
 
 
-# for ubuntu 16.10
-sudo -E apt-get -y install libgtk-3-dev
-sudo -E apt-get -y install libxpm-dev libjpeg-dev libgif-dev libtiff5-dev
-sudo -E apt-get -y install libncurses5-dev
+# for ubuntu 18.04 passed
+sudo -E apt -y install libgtk-3-dev
+sudo -E apt -y install libxpm-dev libjpeg-dev libgif-dev libtiff5-dev
+sudo -E apt -y install libncurses5-dev
 
 
 ./configure --with-modules --without-compress-install
--- installer/setup-for-ubuntu.sh
+++ installer/setup-for-ubuntu.sh
@@ -2,17 +2,17 @@
 #! /bin/sh
 
 # sudo -E add-apt-repository ppa:ubuntu-elisp/ppa
-sudo -E apt-get -y update
+sudo -E apt -y update
 
-sudo -E apt-get -y install gnome-color-chooser
+sudo -E apt -y install gnome-color-chooser
 
-sudo -E apt-get -y install fonts-inconsolata
+sudo -E apt -y install fonts-inconsolata
 
-sudo -E apt-get -y install git
-sudo -E apt-get -y install subversion
-sudo -E apt-get -y install cmake
+sudo -E apt -y install git
+sudo -E apt -y install subversion
+sudo -E apt -y install cmake
 
-sudo -E apt-get -y install build-essential
+# sudo -E apt -y install build-essential
 
 
 mkdir work
